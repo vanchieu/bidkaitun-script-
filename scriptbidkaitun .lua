95cb5faafd2b6cad871a377f7e281a8554fd45d4
vanchieu <chieu13112010@gmail.com>
2024-10-01 22:30:51 +0700

    Add files via upload

---
+++ scriptbidkaitun .lua
@@ -0,0 +1,55 @@
+_G.Quest = { 
+    ['RGB Aura Haki'] = true,
+    ['Pull Lever'] = true,
+    ['Quest Dough Awaken'] = true
+}
+_G.Race = { 
+    ['Select Race'] = {'Human','Fishman','Skypiea','Mink'},
+    ['Lock Race'] = true,
+    ['Evo Race V3'] = true
+}
+_G.Melee = { 
+    ['Godhuman'] = true,
+    ['Electric claw']=true,
+    ['Dragon Talon']=true,
+}
+_G.Sword = { 
+    ['Saber'] = true,
+    ['Midnight Blade'] = true,
+    ['Shisui'] = true,
+    ['Saddi'] = true,
+    ['Wando'] = true,
+    ['Yama'] = true,
+    ['Koko'] = false,
+    ['Rengoku'] = true,
+    ['Canvander'] = true,
+    ['Buddy Sword'] = true,
+    ['Twin Hooks'] = true,
+    ['SpikeyTrident'] = true,
+    ['Hallow Scryte'] = true,
+    ['Dark Dagger'] = true,
+    ['Tushita'] = true,
+    ['True Triple Katana'] = true,
+    ['Cursed Dual Katana'] = true,
+    ['Shark Anchor'] = true
+}
+_G.Gun = {  
+    ['Kabucha'] = true,
+    ['Acidum Rifle'] = true,
+    ['Soul Guitar'] = true, 
+    ['Serpent Bow'] = true
+}
+_G.Fruit = { 
+    ['Main Fruit'] = {'Dough-Dough'},
+    ['Select Fruit'] = {'Flame-Flame','Ice-Ice','Sand-Sand','Dark-Dark','Light-Light','Magma-Magma','Quake-Quake','Spider-Spider'}
+}
+_G.Mastery = { 
+    ['Melee'] = true,
+    ['Sword'] = true,
+    ['Fruit'] = true
+}
+_G.Setting = {
+    ['FPS Booster'] = true,
+    ['Auto Close Ui'] = true
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vanchieu/chieudepzaihub/main/chieudepzaihub%20.txt"))()
